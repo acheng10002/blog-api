044d1f3c074299f8963eceae5b9e53ff04a39d01
Amy Cheng <109977623+acheng10002@users.noreply.github.com>
2025-05-27 21:53:56 -0400

    Update file for project

--- blog-edit/src/pages/EditPost.jsx
+++ blog-edit/src/pages/EditPost.jsx
@@ -17,6 +17,7 @@ const EditPost = () => {
   const { user, token } = useAuth();
   // local state to track any errors that occur during submission
   const [submitError, setSubmitError] = useState(null);
+  const handleLogout = useHandleLogout(navigate);
 
   /* fetches current post by its ID, returns initialValues (the fetched post),
   loading state, and error from the GET request */
@@ -49,13 +50,15 @@ const EditPost = () => {
     }
   };
 
-  if (loading || error) {
-    return <LoadingOrError loading={loading} error={error} label="post" />;
-  }
-  // if no post data, render a fallback message
-  if (!initialValues) return <p>Post not found.</p>;
-
-  const handleLogout = useHandleLogout(navigate);
+  const renderUserPostsButton = (
+    <button
+      onClick={() =>
+        user ? navigate(`/users/${user.id}/posts`) : navigate("/")
+      }
+    >
+      Back to {user ? "Your Posts" : "Home"}
+    </button>
+  );
 
   return (
     <>
@@ -66,22 +69,32 @@ const EditPost = () => {
         onRegister={() => navigate("/users/register")}
         onLogout={handleLogout}
       />
-      {/* if PUT request error, show it above the form */}
-      {submitError && <p className="error">{submitError}</p>}
-      <PostCard
-        // pre-populates with initialValues from GET request
-        initialValues={initialValues}
-        // submits via handleUpdate
-        onSubmit={handleUpdate}
-        buttonLabel="Update Post"
-      />
-      <button
-        onClick={() =>
-          user ? navigate(`/users/${user.id}/posts`) : navigate("/")
-        }
-      >
-        Back to {user ? "Your Posts" : "Home"}
-      </button>
+
+      {loading ? (
+        <>
+          <LoadingOrError loading={loading} error={error} label="post" />
+          {renderUserPostsButton}
+        </>
+      ) : // if no post data, render a fallback message
+      !initialValues ? (
+        <>
+          <p>Post not found.</p>
+          {renderUserPostsButton}
+        </>
+      ) : (
+        <>
+          {/* if PUT request error, show it above the form */}
+          {submitError && <p className="error">{submitError}</p>}
+          <PostCard
+            // pre-populates with initialValues from GET request
+            initialValues={initialValues}
+            // submits via handleUpdate
+            onSubmit={handleUpdate}
+            buttonLabel="Update Post"
+          />
+          {renderUserPostsButton}
+        </>
+      )}
     </>
   );
 };
